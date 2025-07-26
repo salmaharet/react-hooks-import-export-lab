ab5f77801457b15d6dc78c88d7de16563f9dc8cb
salmaharet <salmaharet459@gmail.com>
2025-07-26 21:35:53 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+     <About/>
+      <Home/>
+
+     
     </div>
   );
 }
